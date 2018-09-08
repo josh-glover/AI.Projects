3b231e54fa069c9e13c86ad844344ae6c1e9fbad
Josh Glover <joshuakglover@gmail.com>
2018-09-07 22:35:00 -0400

    Set Up Base Project

---
+++ AI.Projects/AI.Projects.UI/Bootstrapper.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using AI.Projects.UI.Views;
+using Caliburn.Micro;
+
+namespace AI.Projects.UI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            base.OnStartup(sender, e);
+
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
